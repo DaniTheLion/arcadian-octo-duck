848eec23fdece06eb88c3011d31fa49a7061e3e3
Dani Acherkan <dtl.117@gmail.com>
2015-06-10 18:49:03 +0300

    basic form for fetching offers

--- public/javascripts/fyber_app.js
+++ public/javascripts/fyber_app.js
@@ -1,5 +1,15 @@
 'use strict';
 
-angular.module('fyberApp', []).controller('offersCtrl', function ($scope, $location) {
+angular.module('fyberApp', []).controller('offersCtrl', function ($scope, $location, $http) {
+    $scope.offersQuery = {};
 
+    $scope.getOffers = function(){
+        $http.get('/offers.json').
+            success(function(data, status, headers, config) {
+                $scope.offers = data;
+            }).
+            error(function(data, status, headers, config) {
+                // log error
+            });
+    }
 });
